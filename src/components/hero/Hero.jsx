9a2bb6ec40eb98b17d74b74905d213a9ca1938d6
wabbi-khan <kwahab789@gmail.com>
2022-11-04 03:47:55 +0500

    creating hero section

---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import css from "./Hero.module.css";
+import HeroImg from "../../assets/hero.png";
+import { RiShoppingBagFill } from "react-icons/ri";
+import { BsArrowRight } from "react-icons/bs";
+const Hero = () => {
+  return (
+    <div className={css.container}>
+      {/* Lefy side */}
+      <div className={css.h_side}>
+        <span>skin protection cream</span>
+        <div className={css.text2}>
+          <span>Trendy Collection</span>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+            tenetur.
+          </span>
+        </div>
+      </div>
+
+      {/* middle hero image */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}></div>
+        <img src={HeroImg} alt='HeroImg' width={600} />
+        <div className={css.cart2}>
+          <RiShoppingBagFill />
+          <div className={css.signup}>
+            <span>Best Signup offers</span>
+            <div>
+              <BsArrowRight />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
