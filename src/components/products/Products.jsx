757768a2772c9efc96d340818489dbf657b9acf6
wabbi-khan <kwahab789@gmail.com>
2022-11-07 01:41:14 +0500

    products sec

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import css from "./Products.module.css";
+import Plane from "../../assets/plane.png";
+import { ProductsData } from "../../data/products";
 const Products = () => {
-  return <div>Products</div>;
+  const [menuData, setMenuData] = useState(ProductsData);
+  return (
+    <div className={css.container}>
+      <img src={Plane} alt='Plane' />
+      <span>Our Featured Products</span>
+      <div className={css.products}>
+        <ul className={css.menu}>
+          <li>All</li>
+          <li>Skin Care</li>
+          <li>Conditioners</li>
+          <li>Foundations</li>
+        </ul>
+        <div className={css.list}>
+          {menuData.map((product, i) => (
+            <div className={css.product}>Products</div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
