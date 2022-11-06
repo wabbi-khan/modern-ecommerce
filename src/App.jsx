efe969f09ed8bac9256f506c374dd10bbcb0abbc
wabbi-khan <kwahab789@gmail.com>
2022-11-07 00:57:20 +0500

    creating products sec

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
+import Products from "./components/products/Products";
 import Slider from "./components/slider/Slider";
 import Virtual from "./components/virtual/Virtual";
 
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <Slider />
       <Virtual />
+      <Products />
     </div>
   );
 }
---
+++ src/components/products/Products.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import css from "./Products.module.css";
+const Products = () => {
+  return <div>Products</div>;
+};
+
+export default Products;
