5f0c509683f52049012df50aa65f937fe4ba99d0
wabbi-khan <kwahab789@gmail.com>
2022-11-07 00:49:53 +0500

    virtual left sec

--- src/components/virtual/Virtual.jsx
+++ src/components/virtual/Virtual.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import css from "./Virtual.module.css";
+import Shade from "../../assets/shade.png";
+import ReactCompareImage from "react-compare-image";
+import Before from "../../assets/before.png";
+import After from "../../assets/after.png";
 const Virtual = () => {
-  return <div>Virtual</div>;
+  return (
+    <div className={css.virtual}>
+      <div className={css.left}>
+        <span>Virtual Try-On</span>
+        <span>Never Buy The Wrong Shade Again!</span>
+        <span>Try Now!</span>
+        <img src={Shade} alt='shade-img' />
+      </div>
+      <div className={css.right}>
+        <div className={css.wrapper}>
+          <ReactCompareImage leftImage={Before} rightImage={After} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Virtual;
