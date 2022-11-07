eb0cd6d7d95ee3c2833b35416972c2c137da70dd
wabbi-khan <kwahab789@gmail.com>
2022-11-08 00:33:24 +0500

    ok

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import css from "./Products.module.css";
 import Plane from "../../assets/plane.png";
 import { ProductsData } from "../../data/products";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 const Products = () => {
+  const [parent] = useAutoAnimate();
   const [menuData, setMenuData] = useState(ProductsData);
   const filter = (type) => {
     setMenuData(ProductsData.filter((product) => product.type === type));
@@ -18,7 +20,7 @@ const Products = () => {
           <li onClick={() => filter("conditioner")}>Conditioners</li>
           <li onClick={() => filter("foundation")}>Foundations</li>
         </ul>
-        <div className={css.list}>
+        <div className={css.list} ref={parent}>
           {menuData.map((product, i) => (
             <div className={css.product}>
               <div className='left_s'>
