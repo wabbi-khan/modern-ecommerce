e768ffc56a405b3dbe3bb4efb4e6c966d42e93c3
wabbi-khan <kwahab789@gmail.com>
2022-11-09 02:12:34 +0500

    creating footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Logo from "../../assets/logo.png";
+import {} from "@heroicons/react/outline";
+import css from "./Footer.module.css";
+const Footer = () => {
+  return <div>Footer</div>;
+};
+
+export default Footer;
